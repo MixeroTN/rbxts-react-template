e9978012e61944c7e8a82debce2ef6a9bf4f807d
Shiro <shiro@mailo.com>
2024-08-22 14:06:59 +0200

    Update npm script, new modules

---
+++ src/shared/remotes.ts
@@ -0,0 +1,5 @@
+import { BroadcastAction } from "@rbxts/reflex";
+import { Client, createRemotes, namespace, remote, Server, throttleMiddleware } from "@rbxts/remo";
+import { t } from "@rbxts/t";
+
+export const remotes = createRemotes({});
